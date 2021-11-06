9b76ec4825523a6159340e09eb6587216d4b16bf
Ella Tanttu <ellajtanttu@gmail.com>
2021-11-05 17:03:19 -0700

    add final styling, and clear form button

--- js/scripts.js
+++ js/scripts.js
@@ -21,35 +21,6 @@ Pizza.prototype.pizzaPrice = function() {
 
     this.toppings.forEach(function(topping) {
 
-    // switch (topping) {
-    //   case ("Pepperoni"):
-    //     toppingPrice += 1;
-    //     console.log("Pepp: Topping price is " + toppingPrice);
-    //     break;
-    //   case ("Anchovies"):
-    //     toppingPrice += 1;
-    //     console.log("Anch: Topping price is " + toppingPrice);
-    //     break;
-    //   case ("Garlic"):
-    //     toppingPrice += 2;
-    //     console.log("Garlic: Topping price is " + toppingPrice);
-    //     break;
-    //   case ("Onions"):
-    //     toppingPrice += 1;
-    //     console.log("Onions: Topping price is " + toppingPrice);
-    //     break;
-    //   case ("Mushrooms"):
-    //     toppingPrice += 2;
-    //     console.log("Mush: Topping price is " + toppingPrice);
-    //     break;
-    //   case ("Artichoke Hearts"):
-    //     toppingPrice += 3;
-    //     console.log("ArtHearts: Topping price is " + toppingPrice);
-    //     break;
-    //   default:
-    //     break;
-    // }
-
     if (topping === "Pepperoni") {
       toppingPrice += 1;
       console.log("Pepp: Topping price is " + toppingPrice);
@@ -80,61 +51,51 @@ Pizza.prototype.pizzaPrice = function() {
 $(document).ready(function() {
   $("#orderForm").submit(function(event) {
     event.preventDefault();
-    $("#receiptContainer").show();
     const inputPieSize = $("input:radio[name=inlineRadioOptions]:checked").val();
-    console.log("inputPieSize is " + inputPieSize);
-
     let inputToppings = [];
-    $.each($("input[name='toppingName']:checked"), function() {
-      inputToppings.push($(this).val());
-    });
     let userPizza = new Pizza(inputPieSize, inputToppings, 0);
+    if (inputPieSize !== undefined) {
+      $("#oops").hide();
+      $("#receiptContainer").show();
+      $.each($("input[name='toppingName']:checked"), function() {
+        inputToppings.push($(this).val());
+      });
 
-    $("#recTotalPrice").html(userPizza.pizzaPrice());
-    $("#recSize").html(inputPieSize);
-    if (inputPieSize === "Small Pie") {
-      $("#recSizeCost").html(16);
-    } else if (inputPieSize === "Medium Pie") {
-      $("#recSizeCost").html(22);
-    } else {
-      $("#recSizeCost").html(26);
-    }
-    console.log("inputToppings is " + inputToppings);
-    inputToppings.forEach(function(element) {
-      console.log("toppings show loop element is " + element);
-
-
-      // switch (element) {
-      //   case ("Pepperoni"):
-      //     $("#addPeppRec").show();
-      //   case ("Anchovies"):
-      //     $("#addAnchRec").show();
-      //   case ("Garlic"):
-      //     $("#addGarRec").show();
-      //   case ("Onions"):
-      //     $("#addOnionRec").show();
-      //   case ("Mushrooms"):
-      //     $("#addMushRec").show();
-      //   case ("Artichoke Hearts"):
-      //     $("#addArtRec").show();
-      //   default:
-      //     break;
-      // }
-
-      if (element === "Pepperoni") {
-        $("#addPeppRec").show();
-      } else if (element === "Anchovies") {
-        $("#addAnchRec").show();
-      } else if (element === "Garlic") {
-        $("#addGarRec").show();
-      } else if (element === "Onions") {
-        $("#addOnionRec").show();
-      } else if (element === "Mushrooms") {
-        $("#addMushRec").show();
-      } else if (element === "Artichoke Hearts") {
-        $("#addArtRec").show();
+      $("#recTotalPrice").html(userPizza.pizzaPrice());
+      $("#recSize").html(inputPieSize);
+      if (inputPieSize === "Small Pie") {
+        $("#recSizeCost").html(16);
+      } else if (inputPieSize === "Medium Pie") {
+        $("#recSizeCost").html(22);
+      } else {
+        $("#recSizeCost").html(26);
       }
-    });
+      inputToppings.forEach(function(element) {
+
+        if (element === "Pepperoni") {
+          $("#addPeppRec").show();
+        } else if (element === "Anchovies") {
+          $("#addAnchRec").show();
+        } else if (element === "Garlic") {
+          $("#addGarRec").show();
+        } else if (element === "Onions") {
+          $("#addOnionRec").show();
+        } else if (element === "Mushrooms") {
+          $("#addMushRec").show();
+        } else if (element === "Artichoke Hearts") {
+          $("#addArtRec").show();
+        }
+      });
+      $("#clearOrder").click(function(event) {
+        event.preventDefault();
+        $("#receiptContainer").hide();
+        // $(".list-group").empty();
+        document.getElementById("orderForm").reset();
+      })
+
+    } else {
+      $("#oops").show();
+    }
 
   });
 });
